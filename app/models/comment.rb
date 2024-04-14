b6da6b9536e2b237a1a2df2ea4c4d972204a0e74
Rafael Caroni <87575645+kroonus2@users.noreply.github.com>
2024-04-14 19:30:03 -0300

    add concern status comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,7 @@
 class Comment < ApplicationRecord
+  # Inclui o concern 'Visible', que encapsula funcionalidades relacionadas à visibilidade
   include Visible
 
+  # Define a associação 'belongs_to' com o modelo 'Article', indicando que um comentário pertence a um artigo específico
   belongs_to :article
 end
--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -1,18 +1,24 @@
 module Visible
   extend ActiveSupport::Concern
 
+  # Define uma constante para os status válidos
   VALID_STATUSES = ['public', 'private', 'archived']
 
+  # Método chamado quando o módulo é incluído em uma classe
   included do
+    # Define uma validação para o atributo status, garantindo que ele seja um dos status válidos
     validates :status, inclusion: { in: VALID_STATUSES }
   end
 
+  # Métodos de classe definidos dentro do concern
   class_methods do
+    # Método de classe para contar quantos registros têm o status 'public'
     def public_count
       where(status: 'public').count
     end
   end
 
+  # Método de instância para verificar se o status é 'archived'
   def archived?
     status == 'archived'
   end
